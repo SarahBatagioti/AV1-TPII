010259e3f68342889f8beb0ab1bb12f7aa53281c
SarahBatagioti <sarah.montuanibt@gmail.com>
2026-03-19 20:05:28 -0300

    feat: criação de clientes titulares e dependentes com novas validações

--- teste/index.ts
+++ teste/index.ts
@@ -1,27 +1,73 @@
+import { TipoDocumento } from "../enumeracoes/tipoDocumento";
 import Cliente from "../modelos/cliente";
+import Documento from "../modelos/documento";
 import Endereco from "../modelos/endereco";
-let cliente = new Cliente()
-cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
-cliente.nomeSocial = `Dom Pedro II`
-cliente.dataCadastro = new Date(1840, 6, 23)
-cliente.dataNascimento = new Date(1825, 11, 2)
-let endereco = new Endereco()
-endereco.rua = `R. do Catete`
-endereco.bairro = `Copacabana`
-endereco.cidade = `Rio de Janeiro`
-endereco.estado = `Rio de Janeiro`
-endereco.pais = `Brasil`
-endereco.codigoPostal = `22220-000`
-cliente.endereco = endereco
+import Telefone from "../modelos/telefone";
 
-let dependente = new Cliente()
-dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
-dependente.nomeSocial = `Princesa Isabel`
-dependente.dataCadastro = new Date(1921, 10, 14)
-dependente.dataNascimento = new Date(1846, 6, 29)
-dependente.endereco = (cliente.endereco.clonar() as Endereco)
-dependente.titular = cliente
-cliente.dependentes.push(dependente)
+// Endereço Cliente Titular
 
-console.log(cliente);
-console.log(dependente);
+let enderecoVal = new Endereco()
+enderecoVal.rua = `Av. Jupira`
+enderecoVal.bairro = `Paraíso Castanheiras`
+enderecoVal.cidade = `São José dos Campos`
+enderecoVal.estado = `São Paulo`
+enderecoVal.pais = `Brasil`
+enderecoVal.codigoPostal = `12345-000`
+
+// Documento Titular
+
+let documentoVal = new Documento()
+documentoVal.tipo = TipoDocumento.CPF
+documentoVal.dataExpedicao = new Date('2020-12-10')
+documentoVal.numero = '12345678901'
+
+// Telefone Titular
+
+let telefoneVal = new Telefone()
+telefoneVal.ddd = '12'
+telefoneVal.numero = '98179-0173'
+
+// Cliente Titular
+
+let clienteTitular = new Cliente()
+clienteTitular.nome = 'Valdirene Montuani'
+clienteTitular.nomeSocial = 'Val'
+clienteTitular.endereco = enderecoVal
+clienteTitular.documentos = [documentoVal]
+clienteTitular.dataNascimento = new Date('1982-07-13')
+clienteTitular.telefones = [telefoneVal]
+clienteTitular.dataCadastro = new Date('2026-03-19')
+
+// console.log(clienteTitular)
+
+// Cliente dependente
+
+let clienteDependente = new Cliente()
+clienteDependente.nome = 'Sarah Montuani'
+clienteDependente.nomeSocial = 'Sarão'
+clienteDependente.endereco = (clienteTitular.endereco.clonar() as Endereco) // copiar endereço do titular
+clienteDependente.dataNascimento = new Date('2006-03-29')
+clienteDependente.telefones = clienteTitular.telefones.map(t => t.clonar() as Telefone) // copiar telefone do titular
+clienteDependente.titular = clienteTitular // Vincular 'Val' como titular
+clienteDependente.dataCadastro = new Date('2026-03-19')
+
+// Vincular 'Sarão' como dependente
+clienteTitular.dependentes.push(clienteDependente)
+
+// Imprimir o cadastro do titular e dependente
+console.log(clienteTitular)
+console.log(clienteDependente)
+
+// Validação se foi clonado realmente o telefone
+console.log(`Telefone titular: (${clienteTitular.telefones[0].ddd}) ${clienteTitular.telefones[0].numero}`)
+console.log(`Telefone dependente: (${clienteDependente.telefones[0].ddd}) ${clienteDependente.telefones[0].numero}`)
+
+// Deverá dar falso, já que é um cópia - objetos diferentes na memória
+console.log("Mesmo objeto de telefone?", clienteTitular.telefones[0] === clienteDependente.telefones[0])
+
+// Validação de alterar somente o do dependente, para provar independência entre os objetos
+/*clienteDependente.telefones[0].numero = "99999-0000"
+
+console.log("Após alterar dependente:")
+console.log("Número titular:", clienteTitular.telefones[0].numero)
+console.log("Número dependente:", clienteDependente.telefones[0].numero) */
