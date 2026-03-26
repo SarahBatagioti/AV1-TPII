693143fe5974394f47657d4b7ff9eca21d775178
SarahBatagioti <sarah.montuanibt@gmail.com>
2026-03-26 19:55:48 -0300

    feat: integrar a classe Entrada para entrada de dados via terminal

--- teste/entrada.ts
+++ teste/entrada.ts
@@ -1,19 +1,20 @@
 import promptSync from "prompt-sync";
 export default class Entrada {
+    private prompt = promptSync({ sigint: true })
+
     public receberNumero(mensagem: string): number {
-        let prompt = promptSync();
-        let valor = prompt(`${mensagem}: `)
+        let valor = this.prompt(`${mensagem}: `)
         let numero = new Number(valor)
         return numero.valueOf()
     }
+
     public receberTexto(mensagem: string): string {
-        let prompt = promptSync();
-        let texto = prompt(`${mensagem}: `)
+        let texto = this.prompt(`${mensagem}: `)
         return texto
     }
+
     public receberData(mensagem: string): Date {
-        let prompt = promptSync();
-        let texto = prompt(`${mensagem}, no padrão dd/MM/yyyy: `)
+        let texto = this.prompt(`${mensagem} (dd/MM/yyyy): `)
         let partes = texto.split('/')
         let ano = new Number(partes[2])
         let mes = new Number(partes[1])
